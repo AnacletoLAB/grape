7f78a05cec6ef4f6f2fe0356da6b01269874078d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-07 08:49:14 +0200

    Published new version and updated dependencies

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     install_requires=[
-        "ensmallen==0.7.0.dev6",
-        "embiggen==0.10.0.dev3",
+        "ensmallen==0.7.0.dev9",
+        "embiggen==0.10.0.dev6",
     ]
 )
