0a7ee15f52e00a8817868803942b8398c9064532
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-06-15 23:29:41 +0200

    Bumped version

--- grape/__version__.py
+++ grape/__version__.py
@@ -1,2 +1,2 @@
 """Current version of package GraPE."""
-__version__ = "0.1.2"
+__version__ = "0.1.3"
--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     install_requires=[
-        "ensmallen>=0.8.1",
-        "embiggen>=0.11.6",
+        "ensmallen>=0.8.2",
+        "embiggen>=0.11.7",
     ]
 )
