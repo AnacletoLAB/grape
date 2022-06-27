a046833154962269e518fe310e58e94905e2e053
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-06-27 16:38:07 +0200

    Updated requirements and version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     install_requires=[
-        "ensmallen>=0.8.4",
-        "embiggen>=0.11.11",
+        "ensmallen>=0.8.5",
+        "embiggen>=0.11.12",
     ]
 )
