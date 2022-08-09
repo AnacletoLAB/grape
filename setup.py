a12a0d66ccf14d172427d2b042ff386ff8b9b2a5
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-09 09:59:13 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     install_requires=[
-        "ensmallen>=0.8.12",
-        "embiggen>=0.11.23",
+        "ensmallen>=0.8.13",
+        "embiggen>=0.11.24",
     ]
 )
