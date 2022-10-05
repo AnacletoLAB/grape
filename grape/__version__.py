06a41dce3a2426d60606ff2697012024a43f1bf3
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-10-05 19:41:50 +0200

    Updated requiremnts

--- grape/__version__.py
+++ grape/__version__.py
@@ -1,2 +1,2 @@
 """Current version of package GRAPE."""
-__version__ = "0.1.25"
+__version__ = "0.1.26"
--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     install_requires=[
-        "ensmallen>=0.8.27",
+        "ensmallen>=0.8.28",
         "embiggen>=0.11.39",
     ]
 )
