1d4046db9bdb9e3cc3d66725fa92702c28fe17ef
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-11-01 16:29:12 +0100

    Bumped version and version requirements

--- grape/__version__.py
+++ grape/__version__.py
@@ -1,2 +1,2 @@
 """Current version of package GRAPE."""
-__version__ = "0.2.2"
+__version__ = "0.2.3"
--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         "downloaders",
         "bioregistry",
         "py-cpuinfo",
-        "ensmallen>=0.8.64",
-        "embiggen>=0.11.61",
+        "ensmallen>=0.8.88",
+        "embiggen>=0.11.85",
     ]
 )
